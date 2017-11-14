58e75c57078b805730b620e14c4b6d9d3669c92b
mvl22 <martin@lucas-smith.co.uk>
2017-11-14 17:26:43 +0000

    Initial commit, adding framework

---
+++ README.md
@@ -0,0 +1,45 @@
+Conference bookings system
+=========================
+
+This is a PHP application which implements a simple conference bookings system.
+
+
+Screenshot
+----------
+
+![Screenshot](screenshot.png)
+
+
+Usage
+-----
+
+1. Clone the repository.
+2. Download the library dependencies and ensure they are in your PHP include_path.
+3. Download and install the famfamfam icon set in /images/icons/
+4. Add the Apache directives in httpd.conf (and restart the webserver) as per the example given in .httpd.conf.extract.txt; the example assumes mod_macro but this can be easily removed.
+5. Create a copy of the index.html.template file as index.html, and fill in the parameters.
+6. Access the page in a browser at a URL which is served by the webserver.
+
+
+Dependencies
+------------
+
+* [application.php application support library](https://download.geog.cam.ac.uk/projects/application/)
+* [database.php database wrapper library](https://download.geog.cam.ac.uk/projects/database/)
+* [frontControllerApplication.php front controller application implementation library](https://download.geog.cam.ac.uk/projects/frontcontrollerapplication/)
+* [pureContent.php site framework](https://download.geog.cam.ac.uk/projects/purecontent/)
+* [ultimateForm.php form library](https://download.geog.cam.ac.uk/projects/ultimateform/)
+* [FamFamFam Silk Icons set](http://www.famfamfam.com/lab/icons/silk/)
+
+
+Author
+------
+
+Martin Lucas-Smith, Department of Geography, University of Cambridge, 2017.
+
+
+License
+-------
+
+GPL2.
+
---
+++ conferenceBookings.php
@@ -0,0 +1,111 @@
+<?php
+
+# Class to create a conference bookings system
+
+
+require_once ('frontControllerApplication.php');
+class conferenceBookings extends frontControllerApplication
+{
+	# Function to assign defaults additional to the general application defaults
+	public function defaults ()
+	{
+		# Specify available arguments as defaults or as NULL (to represent a required argument)
+		$defaults = array (
+			'applicationName' => 'Conference bookings',
+			'div' => strtolower (__CLASS__),
+			'database' => 'conferencebookings',
+			'table' => 'bookings',
+			'databaseStrictWhere' => true,
+			'administrators' => true,
+			'useEditing' => true,
+		);
+		
+		# Return the defaults
+		return $defaults;
+	}
+	
+	
+	# Function assign additional actions
+	public function actions ()
+	{
+		# Specify additional actions
+		$actions = array (
+			'home' => array (
+				'description' => false,
+				'url' => '',
+				'tab' => 'Home',
+				'icon' => 'house',
+			),
+			'edit' => array (
+				'description' => false,
+				'url' => '%1/edit.html',
+				'usetab' => 'home',
+				'authentication' => true,
+			),
+		);
+		
+		# Return the actions
+		return $actions;
+	}
+	
+	
+	# Database structure definition
+	public function databaseStructure ()
+	{
+		return "
+			CREATE TABLE `administrators` (
+			  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Username' PRIMARY KEY,
+			  `active` enum('','Yes','No') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Yes' COMMENT 'Currently active?'
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='System administrators';
+		";
+	}
+	
+	
+	# Additional initialisation
+	public function main ()
+	{
+		
+	}
+	
+	
+	
+	# Welcome screen
+	public function home ()
+	{
+		# Start the HTML
+		$html = "\n<p>Welcome to the conference booking system.</p>";
+		
+		# Show the HTML
+		echo $html;
+	}
+	
+	
+	# Admin editing section, substantially delegated to the sinenomine editing component
+	public function editing ($attributes = array (), $deny = false, $sinenomineExtraSettings = array ())
+	{
+		# Define sinenomine settings
+		$sinenomineExtraSettings = array (
+			'int1ToCheckbox' => true,
+			'simpleJoin' => true,
+			'datePicker' => true,
+			'richtextEditorToolbarSet' => 'BasicLonger',
+			'richtextWidth' => 600,
+			'richtextHeight' => 200,
+		);
+		
+		# Define table attributes
+		$attributes = array (
+		);
+		
+		# Define tables to deny editing for
+		$deny[$this->settings['database']] = array (
+			'administrators',
+			'settings',
+		);
+		
+		# Hand off to the default editor, which will echo the HTML
+		parent::editing ($attributes, $deny, $sinenomineExtraSettings);
+	}
+}
+
+?>
